0d1ccd984ec74ce4099d00c5f757cf1b7b87e01f
JuMoth <tkdlqjqkd@gmail.com>
2025-07-02 17:42:10 +0900

    style: Unused import Code Delete

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,4 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
