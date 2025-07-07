239350434f864fac9ecda16c7fbf6a92a34b5474
JuMoth <tkdlqjqkd@gmail.com>
2025-07-07 16:04:17 +0900

    feat: Change Search Page to SSG

--- src/pages/book/[id].tsx
+++ src/pages/book/[id].tsx
@@ -1,7 +1,7 @@
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 import style from './[id].module.css';
 import fetchOneBook from '@/lib/fetch-one-book';
-const mockData = {
+/*const mockData = {
     "id": 1,
     "title": "한 입 크기로 잘라 먹는 리액트",
     "subTitle": "자바스크립트 기초부터 애플리케이션 배포까지",
@@ -9,7 +9,7 @@ const mockData = {
     "author": "이정환",
     "publisher": "프로그래밍인사이트",
     "coverImgUrl": "https://shopping-phinf.pstatic.net/main_3888828/38888282618.20230913071643.jpg"
-};
+};*/
 
 export const getServerSideProps = async (context: GetServerSidePropsContext) => {
     const id = context.params!.id;
@@ -29,7 +29,7 @@ export default function Page(
     if(!book) return "문제가 발생했습니다 다시 시도하세요";
 
     const {
-        id,
+        // id,
         title,
         subTitle,
         description,
