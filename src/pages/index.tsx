c3d9e31bf265576fb7ef587652ee904658084634
JuMoth <tkdlqjqkd@gmail.com>
2026-01-28 18:38:30 +0900

    feat: SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { InferGetServerSidePropsType } from "next";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
+
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -24,8 +26,8 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(allBooks);
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  // console.log(allBooks);
 
   return (
     <div className={style.container}>
